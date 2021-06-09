2e5713e423c1a492fc3a435a279b06e062c5dd56
luobotang <tangkangxing@sina.com>
2021-06-09 11:17:21 +0800

    v0.3.2 支持创建DB

--- modules/mysql/README.md
+++ modules/mysql/README.md
@@ -8,14 +8,29 @@
 
 ## API
 
-- ```app.db.escape(val)```: 内部调用 ```mysql.escape(val)```
-- ```app.db.query(sql, params)```: 基础查询方法，返回 Promise，成功数据为 { results, fields }，失败数据为错误信息。
-- ```async app.db.select(sql, params)```: 返回 results
-- ```async app.db.select_one(sql, params)```: 返回 results[0]
-- ```async app.db.insert(sql, params)```: 返回 results.affectedRows
-- ```async app.db.insert_one(sql, params)```: 返回 results.affectedRows[0]
-- ```async app.db.insert_id(sql, params)```: 返回 results.insertId
-- ```async app.db.update(sql, params)```: 返回 results.changedRows
-- ```async app.db.update_one(sql, params)```: 返回 results.changedRows[0]
-- ```async app.db.delete(sql, params)```: 返回 results.affectedRows
-- ```async app.db.delete_one(sql, params)```: 返回 results.affectedRows[0]
+`MySqlDb`:
+
+- ```db.escape(val)```: 内部调用 ```mysql.escape(val)```
+- ```db.query(sql, params)```: 基础查询方法，返回 Promise，成功数据为 { results, fields }，失败数据为错误信息。
+- ```async db.select(sql, params)```: 返回 results
+- ```async db.select_one(sql, params)```: 返回 results[0]
+- ```async db.insert(sql, params)```: 返回 results.affectedRows
+- ```async db.insert_one(sql, params)```: 返回 results.affectedRows[0]
+- ```async db.insert_id(sql, params)```: 返回 results.insertId
+- ```async db.update(sql, params)```: 返回 results.changedRows
+- ```async db.update_one(sql, params)```: 返回 results.changedRows[0]
+- ```async db.delete(sql, params)```: 返回 results.affectedRows
+- ```async db.delete_one(sql, params)```: 返回 results.affectedRows[0]
+
+作为插件自动安装后，即添加 `app.db` 和 `app.context.db`，值为 `MySqlDb` 类型。
+
+也可以手动创建：
+
+```js
+const { createMySqlDb } = require('@luobotang/koa-mysql')
+
+const db = createMySqlDb(
+  { /* mysql config */ },
+  logger /* log4js logger */
+)
+```
--- modules/mysql/index.js
+++ modules/mysql/index.js
@@ -1,61 +1,52 @@
 const mysql = require('mysql')
 
-module.exports = (app) => {
-  const logger = app.getLogger('@luobotang/koa-mysql')
-  const pool = mysql.createPool(app.config.mysql)
-
+function createMySqlDb(config, logger) {
+  const pool = mysql.createPool(config)
   logger.debug('pool created')
 
-  app.db = app.context.db = {
-    escape: (val) => mysql.escape(val),
-    query(sql, params) {
-      return new Promise((resolve, reject) => {
-        pool.query(sql, params, (error, results, fields) => {
-          if (error) {
-            logger.error(error, 'sql:', sql, 'params:', params || '')
-            reject(error)
-            return
-          }
-          resolve({ results, fields })
-        })
+  function query(sql, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(sql, params, (error, results, fields) => {
+        if (error) {
+          logger.error(error, 'sql:', sql, 'params:', params || '')
+          reject(error)
+          return
+        }
+        resolve({ results, fields })
       })
-    },
-    // 简化查询后数据处理的辅助方法
-    async select(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results
-    },
-    async select_one(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results && results[0]
-    },
-    async insert(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.affectedRows
-    },
-    async insert_one(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.affectedRows === 1
-    },
-    async insert_id(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.insertId
-    },
-    async update(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.changedRows
-    },
-    async update_one(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.changedRows === 1
-    },
-    async delete(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.affectedRows
-    },
-    async delete_one(sql, params) {
-      const { results } = await app.db.query(sql, params)
-      return results.affectedRows === 1
+    })
+  }
+
+  function queryHelper(callback) {
+    return async (sql, params) => {
+      const { results } = await query(sql, params)
+      return callback(results)
     }
   }
+
+  return {
+    escape: (val) => mysql.escape(val),
+    query,
+    // 简化查询后数据处理的辅助方法
+    select:       queryHelper(results => results),
+    select_one:   queryHelper(results => results && results[0]),
+    insert:       queryHelper(results => results.affectedRows),
+    insert_one:   queryHelper(results => results.affectedRows === 1),
+    insert_id:    queryHelper(results => results.insertId),
+    update:       queryHelper(results => results.changedRows),
+    update_one:   queryHelper(results => results.changedRows === 1),
+    delete:       queryHelper(results => results.affectedRows),
+    delete_one:   queryHelper(results => results.affectedRows === 1)
+  }
 }
+
+const plugin = (app) => {
+  app.db = app.context.db = createMySqlDb(
+    app.config.mysql,
+    app.getLogger('@luobotang/koa-mysql')
+  )
+}
+
+plugin.createMySqlDb = createMySqlDb // 方便手动创建
+
+module.exports = plugin // 自动安装插件
